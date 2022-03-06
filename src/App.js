aad4e8cdc6d6e63f71fdf8d8ac1d40fb45958673
BenyBalazs <gnuki2000@gmail.com>
2022-03-06 17:04:12 +0100

    Bejelentkezés mezők validálása.

--- src/App.js
+++ src/App.js
@@ -3,28 +3,25 @@ import './App.css';
 import Header from "./components/headerCompnent/headerComponent";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LoginComponent from "./components/loginComponent/loginComponent";
-import { Routes, Route, Link } from "react-router-dom";
+import {Routes, Route, Link} from "react-router-dom";
 import LandingPage from "./components/landingPageComponent/landingPage";
 import RegisterComponent from "./components/registerComponent/registerComponent";
 
 function App() {
-  return (
-      <div className="all-content">
-          <Header/>
-          <div className="dynamic-content">
-              <Routes>
-                  <Route path="/" element={<LandingPage/>}/>
-                  <Route path="/login" element={<LoginComponent/>}/>
-                  <Route path="/register" element={<RegisterComponent/>}/>
-              </Routes>
-          </div>
-      </div>
+    return (
+        <div className="all-content">
+            <Header/>
+            <div className="dynamic-content">
+                <Routes>
+                    <Route path="/" element={<LandingPage/>}/>
+                    <Route path="/login" element={<LoginComponent/>}/>
+                    <Route path="/register" element={<RegisterComponent/>}/>
+                </Routes>
+            </div>
+        </div>
 
 
-
-
-
-  );
+    );
 }
 
 export default App;
--- src/components/loginComponent/loginComponent.js
+++ src/components/loginComponent/loginComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Alert, Button, Col, Container, FloatingLabel, Form, Row} from "react-bootstrap";
+import {Alert, Button, Col, Container, FloatingLabel, Form, Overlay, Row} from "react-bootstrap";
 import './loginComponent.css'
 import '../formStyle.css'
 import {loginFunction} from "./loginFunction";
@@ -11,8 +11,8 @@ class LoginComponent extends React.Component {
     constructor() {
         super();
         this.state = {
-            username: "", password: "", show: false,
-            showError: "d-none" ,errorMsg : "", verboseErrorMsg: ""
+            username: "", password: "", validated: false,
+            showError: "d-none", errorMsg: "", verboseErrorMsg: ""
         }
     }
 
@@ -20,78 +20,82 @@ class LoginComponent extends React.Component {
         return (
 
             <Container id="login-form-container" className="align-content-center" fluid="lg">
-                <Form className="text-center">
-                    <h1 className="mb-5">Bejelentkezés</h1>
+                    <Form className="text-center" noValidate validated={this.state.validated} onSubmit={this.handleLogin}>
+                        <h1 className="mb-5">Bejelentkezés</h1>
 
-                    <Alert className={this.state.showError} variant="danger">
-                        <Alert.Heading>{this.state.errorMsg}</Alert.Heading>
-                        <p>
-                            {this.state.verboseErrorMsg}
-                        </p>
-                    </Alert>
+                        <Alert className={this.state.showError} variant="danger">
+                            <Alert.Heading>{this.state.errorMsg}</Alert.Heading>
+                            <p>{this.state.verboseErrorMsg}</p>
+                        </Alert>
 
-                    <FloatingLabel controlId="floatingInput" label="Felhasználónév" className="mb-3">
-                        <Form.Control type="text" placeholder="Anonimusz"
-                                      onChange={e => this.setState({username: e.target.value})}
-                                      value={this.state.username}/>
-                    </FloatingLabel>
-                    <FloatingLabel controlId="floatingPassword" label="Jelszó" className="mb-3">
-                        <Form.Control type="password" placeholder="Password"
-                                      onChange={e => this.setState({password: e.target.value})}
-                                      value={this.state.password}/>
-                    </FloatingLabel>
+                        <FloatingLabel controlId="floatingInput" label="Felhasználónév" className="mb-3">
+                            <Form.Control type="text" placeholder="Anonimusz"
+                                          onChange={e => this.setState({username: e.target.value})}
+                                          value={this.state.username} required/>
+                            <Form.Control.Feedback type="invalid">A felhasználónév mező nem maradhat üres.</Form.Control.Feedback>
+                        </FloatingLabel>
+                        <FloatingLabel controlId="floatingPassword" label="Jelszó" className="mb-3">
+                            <Form.Control type="password" placeholder="Password"
+                                          onChange={e => this.setState({password: e.target.value})}
+                                          value={this.state.password} required/>
+                            <Form.Control.Feedback type="invalid">A jelszó mező nem maradhat üres.</Form.Control.Feedback>
+                        </FloatingLabel>
 
-                    <Button variant="sailor_blue" size="xxl" className="mb-3"
-                            onClick={this.handleLogin}>
-                        Bejelentkezés
-                    </Button>
+                        <Button variant="sailor_blue" size="xxl" className="mb-3" type="submit">
+                            Bejelentkezés
+                        </Button>
 
-                    <Container>
-                        <Row className="align-items-center">
-                            <Col className="text-start">
-                                Még nem regisztrált?
-                            </Col>
-                            <Col className="text-end">
-                                <RedirectLoginButton buttonName={"Regisztrálok!"} path={"/register"}/>
-                            </Col>
-                        </Row>
-                    </Container>
-                </Form>
+                        <Container>
+                            <Row className="align-items-center">
+                                <Col className="text-start">
+                                    Még nem regisztrált?
+                                </Col>
+                                <Col className="text-end">
+                                    <RedirectLoginButton buttonName={"Regisztrálok!"} path={"/register"}/>
+                                </Col>
+                            </Row>
+                        </Container>
+                    </Form>
             </Container>
         )
     }
 
     handleLogin = e => {
-        console.log(this.state.username);
-        console.log(this.state.password);
-        loginFunction(this.state.username, this.state.password)
-            .then(res => {
-                createAuthTokenCookie(res.data)
-                getAuthToken();
-            })
-            .catch(err => {
+        e.preventDefault()
+        this.setState({validated: true})
+        const form = e.currentTarget;
 
-            if (err.response.status === 400) {
-                console.log(err.response.status)
-                this.setState(
-                    {
-                        showError: "d-visible",
-                        errorMsg: "Hibás bejelentkezési adatok!",
-                        verboseErrorMsg: "Úgy tűnik, hogy hibás adatokat adott meg kérjük ellenőrizze belépési adatait és próbálja újra."
-                    })
-            }
-            else {
-                console.log(err.response.status)
-                this.setState(
-                    {
-                        showError: "d-visible",
-                        errorMsg: "Valami nincs rendben.",
-                        verboseErrorMsg: "Úgy tűnik, hogy valami váratlan hiba történt."
-                    })
-            }
-        });
+        if (form.checkValidity() === true) {
+            console.log(this.state.username);
+            console.log(this.state.password);
+            this.setState({show: true})
+            loginFunction(this.state.username, this.state.password)
+                .then(res => {
+                    createAuthTokenCookie(res.data)
+                    getAuthToken();
+                })
+                .catch(err => {
 
+                    if (err.response.status === 400) {
+                        console.log(err.response.status)
+                        this.setState(
+                            {
+                                showError: "d-visible",
+                                errorMsg: "Hibás bejelentkezési adatok!",
+                                verboseErrorMsg: "Úgy tűnik, hogy hibás adatokat adott meg kérjük ellenőrizze belépési adatait és próbálja újra."
+                            })
+                    } else {
+                        console.log(err.response.status)
+                        this.setState(
+                            {
+                                showError: "d-visible",
+                                errorMsg: "Valami nincs rendben.",
+                                verboseErrorMsg: "Úgy tűnik, hogy valami váratlan hiba történt."
+                            })
+                    }
+                });
 
+        }
     }
 
 }
