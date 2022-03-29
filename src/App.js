7854003852b68b719b8630ad9ca2b9b910716701
BenyBalazs <gnuki2000@gmail.com>
2022-03-29 15:59:08 +0200

    prod server url

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ import FinancesRouterWrapper from "./components/financesComponent/FinancesRouter
 import FinancesEditComponentWrapper from "./components/financesComponent/FinancesEditComponentWarapper";
 import CreateFinancesComponent from "./components/financesComponent/CreateFinancesComponent";
 
-axios.defaults.baseURL = "http://localhost:8080/api";
+axios.defaults.baseURL = "https://ultra-szuper-kiadaskezelo-be.herokuapp.com/api";
 export default class App extends Component {
 
     constructor() {
